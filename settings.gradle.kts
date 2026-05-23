ab2035a0e76a787f19ecad7e3c2a73b7dd13279a
sodove <mixercraft@gmail.com>
2026-05-23 18:12:17 +0500

    chore: gradle wrapper and version catalog

---
+++ settings.gradle.kts
@@ -0,0 +1,20 @@
+rootProject.name = "volty"
+
+enableFeaturePreview("TYPESAFE_PROJECT_ACCESSORS")
+
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+
+dependencyResolutionManagement {
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+include(":composeApp")
