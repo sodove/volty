c9be2daee49e72155f55e23f46ba9beebb87afb3
sodove <mixercraft@gmail.com>
2026-09-03 20:06:32 +0500

    chore: remove obsolete manifest sentinel

--- composeApp/src/commonMain/kotlin/ru/sodovaya/volty/domain/navigation/region/OfflineRegionPackageManifest.kt
+++ composeApp/src/commonMain/kotlin/ru/sodovaya/volty/domain/navigation/region/OfflineRegionPackageManifest.kt
@@ -347,5 +347,4 @@ object OfflineRegionPackageManifestPolicy {
     private val RELEASE_VERSION_PATTERN = Regex("[A-Za-z0-9][A-Za-z0-9._-]{0,63}")
     private val SHA256_PATTERN = Regex("[0-9a-fA-F]{64}")
     private val RESERVED_KEY_IDS = setOf("UNSIGNED_DEV", "UNSIGNED")
-    private const val UNSIGNED_VALUE = "UNSIGNED"
 }
